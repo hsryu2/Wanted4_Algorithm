0ff5c90b0b2039c3e499b254127abe9091d2e40e
hsryu2 <thryu9376@gmail.com>
2026-03-03 15:07:38 +0900

    Node 클래스 / 쿼드 트리 구현 완료.

---
+++ QuadTree/Partition/QuadTree.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Node.h"
+
+class QuadTree
+{
+public:
+	QuadTree(const Bounds& bounds);
+	~QuadTree();
+
+	// 트리에 노드 추가 함수.
+	void Insert(Node* node);
+
+
+	// 질의 함수.
+	std::vector<Node*> Query(Node* queryNode);
+	// void Query(Node* queryNodem std::vector<Node*>& outNodeList);
+
+public:
+	// 트리의 최대 허용 깊이.
+	static const int maxDepth = 5;
+private:
+	// 루트 노드.
+	Node* root = nullptr;
+};
+
